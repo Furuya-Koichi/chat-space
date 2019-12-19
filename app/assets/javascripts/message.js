d0130f4b81557e240041af504408f98b64752261
Furuya-Koichi <seawasekoichi417@icloud.com>
2019-12-19 17:34:31 +0900

    自動更新機能を追加しました

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,33 +1,50 @@
 $(function(){
+
   function buildHTML(message){
-  if (message.image) {
-    var html = `<div class="chat-main__message-list">
-                <div class="user-name">
+  if (message.content && message.image) {
+      var html = ` <div class="message" data-message-id="${message.id}">
+                    <div class="chat-main__message">
+                    <div class="user-name">
+                    ${message.user_name}
+                    </div>
+                    <div class="date-time">
+                    ${message.created_at}
+                    </div>
+                    <div class="comment"></div>
+                    <p class="lower-message__content">
+                    ${message.content}
+                    </p>
+                    <img class="image-btn" src=${message.image} >
+                    </div>
+                    </div>`
+      } else if(message.content) {
+    var html = ` <div class="message" data-message-id="${message.id}">
+                  <div class="chat-main__message">
+                  <div class="user-name">
                   ${message.user_name}
-                </div>
-                <div class="date-time">
+                  </div>
+                  <div class="date-time">
                   ${message.created_at}
-                </div>
-                <div class="comment"></div>
-                <p class="lower-message__content">
+                  </div>
+                  <div class="comment"></div>
+                  <p class="lower-message__content">
                   ${message.content}
-                </p>
-                <img class="image-btn" src=${message.image} >
-               </div>`
-      } else {
-    var html = `<div class="chat-main__message-list">
-                <div class="user-name">
+                  </p>
+                  </div>
+                  </div>`
+      } else if (message.image){
+     var html = ` <div class="message" data-message-id="${message.id}">
+                  <div class="chat-main__message">
+                  <div class="user-name">
                   ${message.user_name}
-                </div>
-                <div class="date-time">
+                  </div>
+                  <div class="date-time">
                   ${message.created_at}
-                </div>
-                <div class="comment"></div>
-                <p class="lower-message__content">
-                  ${message.content}
-                </p>
-                </div>`
-              }
+                  </div>
+                  <img class="image-btn" src=${message.image} >
+                  </div>
+                  </div>`
+              };
     return html
    }
   $('#new_message').on('submit', function(e){
@@ -51,6 +68,42 @@ $(function(){
     })
     .fail(function() {
       alert("メッセージ送信に失敗しました");
+      $('.send-btn').prop('disabled', false);
   });
   })
-})
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = $('.message:last').data('message-id')
+    console.log(last_message_id)
+    $.ajax({
+      //ルーティングで設定した通りのURLを指定
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      console.log(messages)
+      //追加するHTMLの入れ物を作る
+      var insertHTML = '';
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      //メッセージが入ったHTMLに、入れ物ごと追加
+      $('.chat-main__message-list').append(insertHTML);
+      $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  setInterval(reloadMessages, 7000);
+  
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
+});
+
