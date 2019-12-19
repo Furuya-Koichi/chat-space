77f79188c1c809bea27b842049b2c66da506f3f9
Furuya-Koichi <seawasekoichi417@icloud.com>
2019-12-19 18:11:33 +0900

    98行目をalert('error')に書き換えました

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -95,7 +95,7 @@ $(function(){
       $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
     })
     .fail(function() {
-      console.log('alert');
+      alert('error')
     });
   };
   setInterval(reloadMessages, 7000);
