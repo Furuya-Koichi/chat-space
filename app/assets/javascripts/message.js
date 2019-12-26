cf49b25c29f03b77de2579ddf9cf9ae123990486
Furuya-Koichi <seawasekoichi417@icloud.com>
2019-12-26 21:35:39 +0900

    ①日本時間を表示するようにした②最新の投稿がない時に自動で下までスクロールされるのを解消した③細かいビューの修正をした

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -84,15 +84,16 @@ $(function(){
       data: {id: last_message_id}
     })
     .done(function(messages) {
+      console.log(messages)
       //追加するHTMLの入れ物を作る
       var insertHTML = '';
       //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
       $.each(messages, function(i, message) {
         insertHTML += buildHTML(message)
+         //メッセージが入ったHTMLに、入れ物ごと追加
+          $('.chat-main__message-list').append(insertHTML);
+          $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
       });
-      //メッセージが入ったHTMLに、入れ物ごと追加
-      $('.chat-main__message-list').append(insertHTML);
-      $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
     })
     .fail(function() {
       alert('error')
